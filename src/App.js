4c4ba4dcc5b061b3f32976630c70241777d75ce4
Sflaton <suzanne.flaton@gmail.com>
2016-04-26 15:58:41 +0200

    teachers class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Teacher from './Teacher';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World!</h1>
+            <Teacher />
         );
     }
 }
